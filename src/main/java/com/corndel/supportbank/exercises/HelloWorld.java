5f7264b4bddc042a412ec27f9f41d343f99f4daa
Anna Arnold <anna.arnold.96@gmail.com>
2024-09-30 11:01:44 +0100

    fixes exercises 3

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -2,13 +2,20 @@ package com.corndel.supportbank.exercises;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-// import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Parameters;
+
+
 
 @Command(name = "hello", description = "A CLI that prints hello")
 public class HelloWorld implements Runnable{
+
+    @Parameters(index = "0", description = "The name to greet", defaultValue = "World")
+    private String name;
+
     @Override
     public void run() {
-        System.out.println("Hello, World!");
+        String msg = String.format("Hello, %s!", name);
+        System.out.println(msg);
     }
 
 }
