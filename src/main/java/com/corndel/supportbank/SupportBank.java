777964c76a1bf780e331d3076ac4808cfdf8004f
Anna Arnold <anna.arnold.96@gmail.com>
2024-09-30 10:48:29 +0100

    fixes exercise 2

--- src/main/java/com/corndel/supportbank/SupportBank.java
+++ src/main/java/com/corndel/supportbank/SupportBank.java
@@ -1,10 +1,16 @@
 package com.corndel.supportbank;
 
+import com.corndel.supportbank.exercises.HelloWorld;
 import com.github.tomaslanger.chalk.Chalk;
+import picocli.CommandLine;
 
 public class SupportBank {
 
   public static void main(String[] args) {
     System.out.println(Chalk.on("Build your CLI here!").blue());
+
+    CommandLine cli = new CommandLine(new HelloWorld());
+    int exitCode = cli.execute(args);
+    System.exit(exitCode);
   }
 }
--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -1,8 +1,14 @@
 package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 // import picocli.CommandLine.Parameters;
 
-public class HelloWorld {
+@Command(name = "hello", description = "A CLI that prints hello")
+public class HelloWorld implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Hello, World!");
+    }
+
 }
